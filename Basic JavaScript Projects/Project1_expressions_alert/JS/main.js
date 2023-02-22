0d6abbeb40e0191c0a40263130ea2ccaa8c862a6
Killiest <99295116+ChrisKondo4@users.noreply.github.com>
2023-02-22 05:21:46 -0800

    beginning

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,7 @@
+// Defining variables
+var Sent1 = "This is the beginning of the string", Sent2 = " and this is the end of the string";
+// Concatenating the variables
+var Sents = Sent1 + Sent2;
+window.alert(Sent1);
+document.write(Sent2);
+document.write(Sents);
