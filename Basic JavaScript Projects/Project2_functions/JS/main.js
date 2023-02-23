68078652a10b2848229606b0936423bcce94982c
Killiest <99295116+ChrisKondo4@users.noreply.github.com>
2023-02-22 22:43:51 -0800

    adding comments

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,5 +1,9 @@
+// Instatiating the function used for our button text
 function My_First_Function() {
+    // Declaring the variable
     var str = 'This is the button text!';
+    // Concatenating the variable with this other string
     str += ' And I love it!';
+    // Applying the "str" variable to the HTML element "Button_Text"
     document.getElementById('Button_Text').innerHTML = str;
 }
